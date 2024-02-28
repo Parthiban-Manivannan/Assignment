a63ac5db638568230902db019fceb153c36fee59
parthi14199 <parthi14199@gmail.com>
2024-02-28 17:54:43 +0530

    Commit remove unwanted package

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+
+
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options)
