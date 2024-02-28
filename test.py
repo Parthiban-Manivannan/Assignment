cae7ba9059a7704f82d64273752a1bc7aa34ccf2
parthi14199 <parthi14199@gmail.com>
2024-02-28 17:52:22 +0530

    Commit remove unwanted package

--- test.py
+++ test.py
@@ -3,9 +3,6 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
